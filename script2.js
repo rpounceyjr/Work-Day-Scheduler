b494e153558fba6ee263b0ef52594caab28f211c
rpounceyjr <rpounceyjr@gmail.com>
2020-03-13 10:35:59 -0400

    fixed bug where submit button would submit input value in any div by adding a data-id that disabled submit buttons that weren't next to the taskDiv being clicked.  Now I need to fix a bug with the clear button.

--- script2.js
+++ script2.js
@@ -2,9 +2,8 @@ var date = moment().format("MMM Do, YYYY");;
 var dateHeader = $(".date-header").text(date);
 
 var taskDiv = $(".task-div");
-//ELEMENT THAT ISN'T WORKING
-var taskDiv8 = $(".task-div8");
 
+var taskDiv8 = $(".task-div8");
 var taskDiv9 = $(".task-div9");
 var taskDiv10 = $(".task-div10");
 var taskDiv11 = $(".task-div11");
@@ -15,46 +14,84 @@ var taskDiv15 = $(".task-div15");
 var taskDiv16 = $(".task-div16");
 var taskDiv17 = $(".task-div17");
 
-//THIS IS THE ARRAY
+
 var taskDivArray = [taskDiv8, taskDiv9, taskDiv10, taskDiv11, taskDiv12, taskDiv13, taskDiv14, taskDiv15, taskDiv16, taskDiv17]
 // var taskDivArray = [taskDiv9, taskDiv10, taskDiv11, taskDiv12, taskDiv13, taskDiv14, taskDiv15, taskDiv16, taskDiv17]
 
 var taskInput = "";
+
+//========================On/off switch and taskDiv listener that uses it
 //"on/off switch" for the inputListener
 var inputDisabled = "false"
+
+// taskDiv.on("click", function () {
+//     if (inputDisabled === "false") {
+//         taskInput = $("<input>");
+//         $(this).append(taskInput);
+//         inputDisabled = "true"
+//     } else {
+//         return;
+//     }
+// })
+//========================New task/input/submit/button============
+var thisSubmitButton;
 
 taskDiv.on("click", function () {
     if (inputDisabled === "false") {
         taskInput = $("<input>");
         $(this).append(taskInput);
-        inputDisabled = "true"
+        thisSubmitButton = $(this).next();
+        thisSubmitButton.data("disabled", "false");
+        inputDisabled = "true";
+        console.log(inputDisabled);
+        console.log(thisSubmitButton.data("disabled"));
     } else {
         return;
     }
+    thisSubmitButton.on("click", function () {
+        $(this).prev().text(taskInput.val());
+        // this line sets local storage value
+        localStorage.setItem($(this).prev().data("hour"), taskInput.val());
+        inputDisabled = "false";
+        thisSubmitButton.data("disabled", "true")
+        console.log(inputDisabled);
+        console.log(thisSubmitButton.data("disabled"));
+    })
 })
 
+
+
+// console.log(inputDisabled);
+
+
+
+//==========OLD button listener(probably trash)===========
 // listener to add text from input into the calendar
 // $("button").on("click", function () {
 //     $(this).prev().text(taskInput.val());
-    // this line sets local storage value
+// this line sets local storage value
 //     localStorage.setItem($(this).prev().data("hour"), taskInput.val());
 //     taskInput.val("");
 //     inputDisabled = "false";
 //     console.log(inputDisabled);
 // })
 
+//==========buggy submit-button listener========
 //listener to add text from input into the calendar
-$(".submit-button").on("click", function(){
-    $(this).prev().text(taskInput.val());
-    // this line sets local storage value
-    localStorage.setItem($(this).prev().data("hour"), taskInput.val());
-    taskInput.val("");
-    inputDisabled = "false";
+// $(".submit-button").on("click", function(){
+//     $(this).prev().text(taskInput.val());
+//     // this line sets local storage value
+//     localStorage.setItem($(this).prev().data("hour"), taskInput.val());
+//     taskInput.val("");
+//     inputDisabled = "false";
 
-    console.log(inputDisabled);
-})
+//     console.log(inputDisabled);
+// })
+//============================================
+
+
 //listener to clear task
-$(".clear-button").on("click", function(){
+$(".clear-button").on("click", function () {
     $(this).prev().prev().text("");
     //clears localStorage
     localStorage.setItem($(this).prev().data("hour"), "");
@@ -65,26 +102,26 @@ $(".clear-button").on("click", function(){
 var hour = moment().format("HH");
 console.log(hour);
 
-//THIS IS THE LOOP
+
 //for loop to change the background of taskDivs depeneding on time of day
 for (var i = 0; i < taskDivArray.length; i++) {
     if (taskDivArray[i].data("hour") < hour) {
         taskDivArray[i].attr("style", "background-color:lightgray");
-    }else if(taskDivArray[i].data("hour") === Number(hour)){
+    } else if (taskDivArray[i].data("hour") === Number(hour)) {
         taskDivArray[i].attr("style", "background-color:lightgreen");
-    }else{
+    } else {
         (taskDivArray[i].attr("style", "background-color:lightblue"))
-}
+    }
 }
 //function to populate taskDivs with localStorage info
 
-function getItems(){ 
-   
-    for(var i = 0; i < taskDivArray.length; i++){
+function getItems() {
+
+    for (var i = 0; i < taskDivArray.length; i++) {
         var existingTask = localStorage.getItem(taskDivArray[i].data("hour"));
-        if(existingTask === null){
+        if (existingTask === null) {
             taskDivArray[i].text("")
-        }else{
+        } else {
             taskDivArray[i].text(existingTask);
         }
     }
